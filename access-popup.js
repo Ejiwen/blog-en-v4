8263013148d2c0c23c96012ce9c4d4f504e830b0
ejiwen <skibnewrim@gmail.com>
2022-03-01 23:42:45 -0800

    Deleted the access pop up file ... it was a demo fo JHS

--- access-popup.js
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript">
-    document.body.innerHTML += "<h1 class='testclass'>Also good to mention that it works on most of the browsers including Internet Explorer </h1>";
-</script>
