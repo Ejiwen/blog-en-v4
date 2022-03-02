62a97b1191bfbd8b6cbe7ced5cc5c396a300ba75
ejiwen <skibnewrim@gmail.com>
2022-03-01 23:22:54 -0800

    access hs pop up v1

---
+++ access-popup.js
@@ -0,0 +1,3 @@
+<script type="text/javascript">
+    document.body.innerHTML += "<h1 class='testclass'>Also good to mention that it works on most of the browsers including Internet Explorer </h1>";
+</script>
