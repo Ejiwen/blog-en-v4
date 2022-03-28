18712eed86a02250a5f5913927dbb73155dab10d
ejiwen <skibnewrim@gmail.com>
2022-03-28 00:51:33 -0700

    localStorage v6

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,8 @@ import ThemeContext from "../components/context";
 
 export default function Home({ data }) {
   // top of App function body
-  const theme = useState(localStorage.getItem('theme') == 'light-theme' ? "dark-theme" : "light-theme");
+  //const theme = useState(windowGlobal.localStorage.getItem('theme') == 'light-theme' ? "dark-theme" : "light-theme");
+  const theme = useState(theme == 'light-theme' ? "dark-theme" : "light-theme");
 
 
   return (
